a9bda841c3318ce055fc2657a1db7e940335c81d
dell <dell@localhost.localdomain>
2016-02-11 20:57:11 +0100

    Pseudo-code dans un Main vide

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+
+		/*
+		 -Pseudo code du main:
+		 
+		 -Input -> listes qu'il faut
+		 
+		 Boucle sur toute commandes:
+		 -Lecture d'une commande
+		 	Boucle jusqu'à tout produits trouvés dans des entrepots + en + loin
+		 -Recherche de l'entrepot plus proche contenant au moins 1 produit commandé
+		 -Demande au drone libre le plus proche de venir le chercher (retiré de l'entrepot)
+		   , puis de le livrer (retirer du drone) 
+		 -Supprime la commande de la liste 
+		  
+		  
+		  -Mouvements et chargements/déchargements transcrits en ordres -> Output
+		 
+		 */
+	}
+
+}
